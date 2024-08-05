2f2158d1cda1d8704f4c746d03de9088177fc02c
chrisdiaz9 <chrisdiazbusiness@gmail.com>
2024-08-05 09:16:21 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Navbar.js";
 import Form from "./Form.js";
+import Footer from "./Footer.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Form />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-wrapper">
+        <div className="twitter-link">
+          <p>
+            <a href="https://x.com/chris_dia_z">Follow me!</a>
+          </p>
+        </div>
+        <div>
+          <p>Christopher Diaz @2024</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
