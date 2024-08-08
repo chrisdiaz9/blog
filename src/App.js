efa6cf28538d2c1c73f273ba66135e8d0b550d7e
Chris <chrisdiazbusiness@gmail.com>
2024-08-08 14:59:18 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import "./App.css";
 import Navbar from "./Navbar.js";
 import Form from "./Form.js";
+import BlogPost from "./Post.js";
 import Footer from "./Footer.js";
+
+const posts = [
+  {
+    id: 1,
+    title: "My first post",
+    content: "test post",
+    date: "August 8th 2024",
+  },
+  {
+    id: 2,
+    title: "My second post",
+    content: "test post",
+    date: "August 8th 2024",
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Form />
+      <h1>My pieces of work</h1>
+      {posts.map((post) => (
+        <BlogPost
+          key={post.id}
+          title={post.title}
+          content={post.content}
+          date={post.date}
+        />
+      ))}
+
       <Footer />
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -1,2 +1,14 @@
 import React from "react";
 import "./Post.css";
+
+const BlogPost = ({ title, content, date }) => {
+  return (
+    <div className="blog-post">
+      <h2>{title}</h2>
+      <p>{date}</p>
+      <div>{content}</div>
+    </div>
+  );
+};
+
+export default BlogPost;
