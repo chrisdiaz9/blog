ee913959a63b7898d9aae7808769dea322afcc9d
Chris <chrisdiazbusiness@gmail.com>
2024-08-09 11:16:22 -0700

    See prev commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import "./App.css";
-import Navbar from "./Navbar.js";
 import Form from "./Form.js";
-import Footer from "./Footer.js";
-
-const posts = [
-  {
-    id: 1,
-    title: "My first post",
-    content: "test post",
-    date: "August 8th 2024",
-  },
-  {
-    id: 2,
-    title: "My second post",
-    content: "test post",
-    date: "August 8th 2024",
-  },
-];
+import MainLayout from "./layout/MainLayout.js";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Form />
-      <Footer />
-    </div>
+    <MainLayout>
+      <div className="App">
+        <Form />
+      </div>
+    </MainLayout>
   );
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,22 @@
 import React from "react";
-import Navbar from "../Navbar";
+import "./About.css";
 import MainLayout from "../layout/MainLayout";
 
 const About = () => {
   return (
     <MainLayout>
-      <div>Write about myself here.</div>
+      <div>
+        <p>
+          I'm Chris, and probably just like you, I have a goal I'm working
+          towards. Mine is to achieve personal and financial independence. If
+          you think I'm cool I assure you I'm not. I'm only 20 years old and
+          I've already failed at 10+ projects. And to be real, I haven't
+          succeeded once. The idea of this blog is to document my journey. Each
+          project I work on will be documented here. I'll also write essays here
+          expressing my thoughts and ideas. This is my journey presented to you,
+          I hope you stick along and enjoy!
+        </p>
+      </div>
     </MainLayout>
   );
 };
